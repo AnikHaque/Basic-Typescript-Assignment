d7d76b419a96142346cf4812ac84210f91a474eb
AnikHaque <ekramul15-811@diu.edu.bd>
2025-05-08 23:11:26 +0600

    problem 2 fully done

--- src/assignment.ts
+++ src/assignment.ts
@@ -5,3 +5,17 @@ function formatString(input: string, toUpper: boolean = true): string {
 formatString("Hello");
 formatString("Hello", true);
 formatString("Hello", false);
+
+function filterByRating(
+  items: { title: string; rating: number }[]
+): { title: string; rating: number }[] {
+  return items.filter((item) => item.rating >= 4);
+}
+
+const books = [
+  { title: "Book A", rating: 4.5 },
+  { title: "Book B", rating: 3.2 },
+  { title: "Book C", rating: 5.0 },
+];
+
+filterByRating(books);
